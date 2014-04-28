3731a73b4f096856de9d913e724c37905d6897ee
JC Brand <jc@opkode.com>
2014-04-28 14:19:58 +0200

    Tests now run

--- tests/TestRunner.js
+++ tests/TestRunner.js
@@ -1,22 +1,44 @@
 require.config({
     paths: {
-        "jasmine": "../src/bower_components/jasmine/lib/jasmine-core/jasmine",
-        "jasmine-html": "../src/bower_components/jasmine/lib/jasmine-core/jasmine-html",
-        "console-runner": "../node_modules/phantom-jasmine/lib/console-runner"
+        "console-runner": "../node_modules/phantom-jasmine/lib/console-runner",
+        "jasmine": "../bower_components/jasmine/lib/jasmine-core/jasmine",
+        "jasmine-html": "../bower_components/jasmine/lib/jasmine-core/jasmine-html",
+        "jquery": "../bower_components/jquery/jquery",
+        "jquery.fullcalendar": "../bower_components/fullcalendar/fullcalendar/fullcalendar.min",
+        "jquery.fullcalendar.dnd": "../bower_components/fullcalendar/lib/jquery-ui.custom.min",
+        "logging": "../bower_components/logging/src/logging",
+        "moment": "../bower_components/moment/moment",
+        "moment-timezone": "../bower_components/moment-timezone/moment-timezone",
+        "pat-calendar": "../src/calendar",
+        "pat-calendar-moment-timezone-data": "../src/moment-timezone-data",
+        "pat-compat": "../bower_components/patternslib/src/core/compat",
+        "pat-jquery-ext": "../bower_components/patternslib/src/core/jquery-ext",
+        "pat-logger": "../bower_components/patternslib/src/core/logger",
+        "pat-parser": "../bower_components/patternslib/src/core/parser",
+        "pat-registry": "../bower_components/patternslib/src/core/registry",
+        "pat-store": "../bower_components/patternslib/src/core/store",
+        "pat-utils": "../bower_components/patternslib/src/core/utils"
     },
     shim: {
         "jasmine-html": {
             deps: ["jasmine"],
             exports: "jasmine"
+        },
+        "jquery": {
+            exports: "jQuery"
+        },
+        "jquery.fullcalendar.dnd": {
+            deps: ["jquery"]
         }
     }
 });
 
 define("TestRunner", function() {
     require([
-        "jquery",
-        "jasmine-html"
-    ], function($, jasmine) {
+        "jasmine-html",
+        "logging",
+        "pat-calendar"
+    ], function(jasmine) {
         require([
             "console-runner",
             "specs/calendar.js"
