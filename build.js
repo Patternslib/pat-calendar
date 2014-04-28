013ead64608431ef09ab125413f9688ef46c77b9
JC Brand <jc@opkode.com>
2014-04-28 11:53:30 +0200

    Add build file

---
+++ build.js
@@ -0,0 +1,34 @@
+({
+    baseUrl: "src",
+    out: "bundle.js",
+    name: "almond",
+    include: "patterns",
+
+    wrap: {
+        endFile: "src/wrap-end.js"
+    },
+
+    paths: {
+        // Externals
+        "almond": "bower_components/almond/almond",
+        "jquery": "bower_components/jquery/jquery",
+        "logging": "bower_components/logging/src/logging",
+        "jquery.fullcalendar": "bower_components/fullcalendar/fullcalendar/fullcalendar.min",
+        "jquery.fullcalendar.dnd": "bower_components/fullcalendar/lib/jquery-ui.custom.min",
+        // Calendar pattern
+        "moment": "bower_components/moment/moment",
+        "moment-timezone": "bower_components/moment-timezone/moment-timezone",
+        "pat-calendar": "src/calendar",
+        "pat-calendar-moment-timezone-data": "src/moment-timezone-data"
+    },
+
+    shim: {
+        "jquery": {
+            exports: "jQuery"
+        },
+        "jquery.fullcalendar.dnd": {
+            depends: "jQuery",
+        },
+    },
+    optimize: "none"
+})
